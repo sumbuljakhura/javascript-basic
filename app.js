f04a44d47c245de164000bf765c9dfcd6fa005d2
sumbul mohammad saleem Jakhura <62174359+sumbuljakhura@users.noreply.github.com>
2026-01-06 11:43:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+for(let i=0; i<=100;i++){
+    console.log(i)
+    // document.getElementById('loop').innerHTML=i;
+}
+for(let a=1; a<=10;a++
+){
+    if(a%2==0){
+        console.log(a)
+    }
+    else("odd")
+}
+//2x1=2-----2x10=20
+for(let i=1;i<=10;i++){
+    console.log("2x"+i+"="+i*2)
+}
+let table=Number(prompt("enter your name"))
+let lened=Number(prompt("enter table lenght"))
+for(let i=1;i<=lened;i++){
+    console.log(table+"x"+i+"="+i*table)
+}
+let a=0;
+while(a<=10){
+    console.log(a)
+    a++
+}
